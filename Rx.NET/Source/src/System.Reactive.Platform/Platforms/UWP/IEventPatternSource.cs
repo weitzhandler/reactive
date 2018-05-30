ba451c824d80d6b481962b7a03d1fe72e628a049
Jérôme Laban <jerome.laban@nventive.com>
2018-05-30 09:13:48 -0400

    Add more UWP-specific APIs for Uno.

--- Rx.NET/Source/src/System.Reactive.Platform/Platforms/UWP/IEventPatternSource.cs
+++ Rx.NET/Source/src/System.Reactive.Platform/Platforms/UWP/IEventPatternSource.cs
@@ -2,7 +2,7 @@
 // The .NET Foundation licenses this file to you under the Apache 2.0 License.
 // See the LICENSE file in the project root for more information. 
 
-#if HAS_WINRT
+#if HAS_WINRT || HAS_UNO
 using Windows.Foundation;
 
 namespace System.Reactive
